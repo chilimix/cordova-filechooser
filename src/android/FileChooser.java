57eb13cc84678153d4169d328b2e50b002b30037
Claus <claus@worklogger.io>
2019-03-01 23:20:40 +0100

    ..

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -108,18 +108,12 @@ public class FileChooser extends CordovaPlugin {
     }
 
     private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
-        Cursor cursor = null;
         final String column = "_data";
         final String[] projection = { column };
-        try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
-            if (cursor != null && cursor.moveToFirst()) {
-                final int column_index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(column_index);
-            }
-        } finally {
-            if (cursor != null)
-                cursor.close();
+        final Cursor cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+        if (cursor != null && cursor.moveToFirst()) {
+            final int column_index = cursor.getColumnIndexOrThrow(column);
+            return cursor.getString(column_index);
         }
         return null;
     }
