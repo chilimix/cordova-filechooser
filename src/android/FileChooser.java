657c3453907b303292fdbbdbef65fc3c0a1cd1b5
Claus <claus@worklogger.io>
2019-03-01 22:12:27 +0100

    converting content path to file path

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -30,9 +30,6 @@ public class FileChooser extends CordovaPlugin {
     }
 
     public void chooseFile(CallbackContext callbackContext) {
-
-        // type and title should be configurable
-
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("*/*");
         intent.addCategory(Intent.CATEGORY_OPENABLE);
@@ -50,42 +47,89 @@ public class FileChooser extends CordovaPlugin {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         if (requestCode == PICK_FILE_REQUEST && callback != null) {
-
             if (resultCode == Activity.RESULT_OK) {
                 if (data != null) {
                    String uris = "";
                    if (data.getClipData() != null) {
                       for (int i = 0; i < data.getClipData().getItemCount(); i++) {
-                         uris += data.getClipData().getItemAt(i).getUri().toString() + ", ";
+                         uris += resolveNativePath(data.getClipData().getItemAt(i).getUri()) + ", ";
                       }
                    } else {
-                      uris = data.getData().toString();
+                      uris = resolveNativePath(data.getData());
                    }
                    callback.success(uris);
                 } else {
                     callback.error(resultCode);
                 }
-            // }
-            // if (resultCode == Activity.RESULT_OK) {
-            //     Uri uri = data.getData();
-            //     if (uri != null) {
-            //         Log.w(TAG, uri.toString());
-            //         callback.success(uri.toString());
-            //     } else {
-            //         callback.error("File uri was null");
-            //     }
             } else if (resultCode == Activity.RESULT_CANCELED) {
-
-                // TODO NO_RESULT or error callback?
                 PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
                 callback.sendPluginResult(pluginResult);
-
             } else {
-
                 callback.error(resultCode);
             }
         }
+    }
+
+    private String resolveNativePath(uri) throws JSONException {
+        Uri pvUrl = Uri.parse(uri);
+        Context appContext = this.cordova.getActivity().getApplicationContext();
+        String filePath = getPath(appContext, pvUrl);
+        if (filePath == GET_PATH_ERROR_ID) {
+            return "code://" + GET_PATH_ERROR_CODE;
+        }
+        else if (filePath.equals(GET_CLOUD_PATH_ERROR_ID)) {
+            return "code://" + GET_CLOUD_PATH_ERROR_CODE;
+        }
+        else {
+            return "file://" + filePath;
+        }
+    }
+
+    private static String getPath(final Context context, final Uri uri) {
+        //final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+        // if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+        //     if (isMediaDocument(uri)) {
+        //         final String docId = DocumentsContract.getDocumentId(uri);
+        //         final String[] split = docId.split(":");
+        //         final String type = split[0];
+        //         Uri contentUri = null;
+        //         if ("image".equals(type)) {
+        //             contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        //         } else if ("video".equals(type)) {
+        //             contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+        //         } else if ("audio".equals(type)) {
+        //             contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        //         }
+        //         final String selection = "_id=?";
+        //         final String[] selectionArgs = new String[] { split[1] };
+        //         return getDataColumn(context, contentUri, selection, selectionArgs);
+        //     }
+        // }
+        // else 
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = { column };
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
     }
 }
